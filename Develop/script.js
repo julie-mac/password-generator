5f69b427a9d5a218b03b212c9f2225f551d4eac4
julie-mac <123594716+julie-mac@users.noreply.github.com>
2023-03-21 22:06:52 -0400

    finished javascript code, added commentary to javascript as well

--- Develop/script.js
+++ Develop/script.js
@@ -3,25 +3,26 @@
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '`', '~', '[', ']', '{', '}', '|', ',', '<', '>', '.', '?', '/']
-
-
+var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '`', '~', '[', ']', '{', '}', '|', ',', '<', '>', '.', '?', '/', ' ']
 
 
 function generatePassword() {
+
+  //Storing the chosen criteria in an object, these will be changed later by the user 
   var chosenCriteria = {
       passLength : "",
-      useLowercase : "",
-      useUppercase : "",
-      useNumbers : "",
-      useSymbols : ""
+      useLowercase : null,
+      useUppercase : null,
+      useNumbers : null,
+      useSymbols : null
     }
   
+  //Asks the user how long they would like their password to be
   var askLength = prompt("Please enter your desired length of your password, a number between 8 and 128.");
-
   Number(askLength);
-  console.log(askLength);
 
+  //stores the value given by the user in the chosenCriteria object
+  //If the user enters a number over 128 or less than 8, an alert shows asking to try again
   if (askLength <= 128 && askLength >= 8) {
     chosenCriteria.passLength = askLength;
   } else if (askLength > 128 || askLength < 8) {
@@ -31,44 +32,21 @@ function generatePassword() {
     return;
   }
 
-  var confirmLowercase = confirm("Would you like to include lower case letters in your password? Click OK for Yes, and Cancel for No.");
+  //These confirm prompts ask the use if they would like to include certain criteria
+  chosenCriteria.useLowercase = confirm("Would you like to include lower case letters in your password? Click OK for Yes, and Cancel for No.");
 
-  if (confirmLowercase) {
-    chosenCriteria.useLowercase = true;
-  } else {
-    chosenCriteria.useLowercase = false;
-  }
+  chosenCriteria.useUppercase = confirm("Would you like to include upper case letters in your password? Click OK for Yes, and Cancel for No.");
 
-  var confirmUppercase = confirm("Would you like to include upper case letters in your password? Click OK for Yes, and Cancel for No.");
+  chosenCriteria.useNumbers = confirm("Would you like to include numbers 0-9 in your password? Click OK for Yes, and Cancel for No.");
 
-  if (confirmUppercase) {
-    chosenCriteria.useUppercase = true;
-  } else {
-    chosenCriteria.useUppercase = false;
-  }
-
-  var confirmNumbers = confirm("Would you like to include numbers 0-9 in your password? Click OK for Yes, and Cancel for No.");
-
-  if (confirmNumbers) {
-    chosenCriteria.useNumbers = true;
-  } else {
-    chosenCriteria.useNumbers = false;
-  }
-
-  var confirmSymbols = confirm("Would you like to include symbols in your password? Click OK for Yes, and Cancel for No.");
-
-  if (confirmSymbols) {
-    chosenCriteria.useSymbols = true;
-  } else {
-    chosenCriteria.useSymbols = false;
-  }
-
-  console.log(chosenCriteria)
+  chosenCriteria.useSymbols = confirm("Would you like to include symbols in your password? Click OK for Yes, and Cancel for No.");
   
+  //starting off the characters chosen and password as empty values, to be added onto based on the user's choices
   let characters = [];
   let password = "";
 
-  if (chosenCriteria.useLowercase0) {
+  //Conditional statements to concat the arrays containing different characters, based off user's choices
+  if (chosenCriteria.useLowercase) {
     characters = characters.concat(lowerCase);
   }
   
@@ -77,14 +55,29 @@ function generatePassword() {
   }
 
   if (chosenCriteria.useNumbers) {
-    characters = characters.concat(useNumbers);
+    characters = characters.concat(numbers);
   }
 
   if (chosenCriteria.useSymbols) {
     characters = characters.concat(symbols);
-  }
+  } else if (
+    !chosenCriteria.useLowercase &&
+    !chosenCriteria.useUppercase &&
+    !chosenCriteria.useNumbers &&
+    !chosenCriteria.useSymbols
+    ) {
+      alert("You haven't included any character sets to put in your password. You need to include at least one. Please try again.");
+      return;
+    } //If the user has said 'No' or 'Cancel' for each set of characters, an alert pops up asking them to try again, as they need to include at least one character set.
 
-  console.log(characters)
+
+  //The for loop adds characters from the set of chosen characters to the password, repeats as long as the length of the password chosen by the user
+  for (i = 0 ; i < chosenCriteria.passLength ; i++) {
+    var randomChar = characters[Math.floor(Math.random() * characters.length)];
+    password = password + randomChar;
+  }
+  //Returns the final password to the global scope, to be displayed to the user
+  return password;
 }
 
 
@@ -96,7 +89,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = "Here is your password: " + password;
 
 }
 
